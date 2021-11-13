e20f7a07e1e5c7dbb3e1958b6cadd7891609d69e
JavaWolf26 <77441182+JavaWolf26@users.noreply.github.com>
2021-11-13 18:02:10 +0700

    +

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -17,7 +17,8 @@ public class MainTest {
 
     @Test
     public void mainTest() {
-        String text = "На свете есть океан , на океане остров , на острове дерево , на дереве заяц , в зайце утка , в утке яйцо , в яйце иголка , смерть Кощея на игле :(";
+        String text = "На свете есть океан, в океане остров, на острове дерево, на дереве заяц, в зайце утка, " +
+                "в утке яйцо, в яйце иголка, на игле - смерть Кощея.";
 
         KoscheiTheDeathless koscheiTheDeathless =
                 applicationContext.getBean(KoscheiTheDeathless.class);
